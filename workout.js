9a6a4d043465cd977f639725dd7b30809d7dac7f
Dorian Sanchez <dvs52@nau.edu>
2025-03-21 17:15:09 -0700

    Added workout editing functionality

--- workout.js
+++ workout.js
@@ -1,4 +1,8 @@
-function workoutPopup() {   // creates a new popup for workouts
+function workoutPopup(name, desc, category, type) {
+    let givenName = name || "";
+    let givenDesc = desc || "";
+    let givenCategory = category || "";
+    
     document.getElementById("popup-content").innerHTML = `
         <div>
             <button id="popup-close-btn" class="close-btn">Close</button>
@@ -6,23 +10,25 @@ function workoutPopup() {   // creates a new popup for workouts
                 <div id="workout-title"><h2>Workouts: </h2></div>
                 <div id="workout-editor" class="workout-tab">
                     <h3>Add Workouts</h3>
-                    <label for="name">Workout Name</label>
-                    <br>
-                    <textarea id="workout-name" name="name" maxlength="16" placeholder="Name workout here..." required></textarea>
+                    <form id="workout-form">
+                        <label for="workout-name">Workout Name</label>
+                        <br>
+                        <textarea id="workout-name" name="workout-name" maxlength="16" placeholder="Name workout here..." required>${givenName}</textarea>
+                        
+                        <br>
+                        <label for="workout-desc">Description:</label>
+                        <br>
+                        <textarea id="workout-desc" name="workout-desc" rows="6" cols="30" maxlength="255" placeholder="Describe your workout here..." required>${givenDesc}</textarea>
 
-                    <br>
-                    <label for="desc">Description:</label>
-                    <br>
-                    <textarea id="workout-desc" name="desc" rows="6" cols="30"  maxlength="255" placeholder="Describe your workout here..." required></textarea>
-
-                    <br>
-                    <label for="category">Category:</label>
-                    <br>
-                    <textarea id="workout-category" name="category" rows="3" cols="30" maxlength="255" placeholder="Add categories here separated by spaces"></textarea>
-                    
-                    <br>
-                    <input type="submit" value="Add Workout" id="workout-submit">
-                    </div>
+                        <br>
+                        <label for="workout-category">Category:</label>
+                        <br>
+                        <textarea id="workout-category" name="workout-category" rows="3" cols="30" maxlength="255" placeholder="Add categories here separated by spaces">${givenCategory}</textarea>
+                        
+                        <br>
+                        <input type="submit" value="Add Workout" id="workout-submit">
+                    </form>
+                </div>
                 <div id="workout-lib" class="workout-tab">
                     <h3>Workout Library</h3>
                 </div>
@@ -30,12 +36,37 @@ function workoutPopup() {   // creates a new popup for workouts
         </div>
     `;
 
-    document.getElementById("popup-close-btn").addEventListener("click", closePopup);   // add purpose to close button
-    document.getElementById("popup").style.display = "block";   // add styling to popup
-    document.getElementById("overlay-bg").style.display = "block";  // add styling to bg
-    document.getElementById("workout-submit").addEventListener("click", () => addWorkout()) // set submit button to call adding workout function
-    showWorkoutLibrary()    // show the workout library
+    // Close button functionality
+    document.getElementById("popup-close-btn").addEventListener("click", closePopup);
+    document.getElementById("popup").style.display = "block";   // display popup
+    document.getElementById("overlay-bg").style.display = "block";  // display background overlay
+    
+    const form = document.getElementById("workout-form");
+
+    form.onsubmit = function(event) {
+        if (form.checkValidity()) {
+            event.preventDefault();  // prevent the form from submitting
+
+            // check if name and desc are undefined
+            if (name !== undefined && desc !== undefined) {
+                editWorkout(type);  // edit workout
+            } else {
+                addWorkout();  // add workout
+            }
+        } else {
+            console.log("Form is not valid.");
+            event.preventDefault();  // prevent form submission
+        }
+    };
+
+    // change button if editing workout
+    if (name !== undefined && desc !== undefined) {
+        document.getElementById("workout-submit").value = "Edit Workout";
+    }
+
+    showWorkoutLibrary();  // show the workout library
 }
+
 
 function addWorkout() {
     // gets the new workout information
@@ -92,12 +123,48 @@ function showWorkoutLibrary() {
             if (category !== "") {
                 element += `<p class="workout-text">Categories: ${category}</p>`;   // add category if listed
             }
+            
+            // add edit and remove buttons
+            element += `
+                <button class="edit-Task" id="edit-workout-${i}">edit</button>`
+                // <button class="remove-Task" id="remove-workout-${i}">remove</button>     remove this comment and move the tilde when adding the remove functionality
+
             // add to elements
             workout.innerHTML = element;    // add html to new div
             document.getElementById("workout-lib").appendChild(workout) // add new div to workouts tab
+
+            // add functionality to the buttons
+            document.getElementById(`edit-workout-${i}`).addEventListener("click", () => editWorkoutsSetup(i))
+            // document.getElementById(`remove-workout-${i}`).addEventListener("click", () => removeWorkout(i))     // remove comment when implementing remove functionality
         }
     } else {
         // if there arn't any workouts, append no workouts message
         document.getElementById("workout-lib").innerHTML += "No workouts to show!<br>Try adding some new workouts to see them here!"
     }
+}
+
+function editWorkoutsSetup(type) {
+    // get data for edited workout
+    var name = localStorage.getItem(`workout-name-${type}`);
+    var desc = localStorage.getItem(`workout-desc-${type}`);
+    var category = localStorage.getItem(`workout-categories-${type}`);
+
+    // reload workout popup with information given
+    workoutPopup(name, desc, category, type)
+}
+
+function editWorkout(type) {
+    // store edited workout
+    // get new values
+    let name = document.getElementById("workout-name").value;
+    let desc = document.getElementById("workout-desc").value;
+    let category = document.getElementById("workout-category").value;
+
+    // set new values
+    localStorage.setItem(`workout-name-${type}`, name)
+    localStorage.setItem(`workout-desc-${type}`, desc)
+    localStorage.setItem(`workout-categories-${type}`, category)
+
+    // call popup to update information
+    workoutPopup()
 }
